6a020f030fdfa2bed45381cb544e77b96693a9e3
BolverBlitz <bolverblitz@ebg.pw>
2021-11-22 01:03:08 +0100

    Fixed some bugs, added example dashboard

--- src/collectStats.js
+++ src/collectStats.js
@@ -5,87 +5,101 @@ const DBDelay = Number(process.env.CheckDelayInMS)/1000;
 let StatsCollector = function() {
 	return new Promise(function(resolve, reject) {
         const Promisees = [db.GetAllUsers(), db.GetAllServers(), db.GetAllProducts(), db.GetFailedJobsSinceLastCheck(DBDelay)];
+        try {
+            Promise.all(Promisees).then(function(values) {
+                const Users = values[0];
+                const Servers = values[1];
+                const Products = values[2];
+                const FailedJobs = values[3];
 
-        Promise.all(Promisees).then(function(values) {
-            const Users = values[0];
-            const Servers = values[1];
-            const Products = values[2];
-            const FailedJobs = values[3];
+                let [TotalCoins, TotalSuspendedUsers, TotalAllowedServers, TotalVerifiedUsers, TotalVerifiedDiscordUsers, TotalCPU, TotalRAM, TotalSWAP, TotalDisk, TotalAllocations, TotalDB, TotalServerCostMonthly, ServerSuspended] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                let UserCountrys = [];
+                let ServerProducts = [];
 
-            let [TotalCoins, TotalSuspendedUsers, TotalAllowedServers, TotalVerifiedUsers, TotalVerifiedDiscordUsers, TotalCPU, TotalRAM, TotalSWAP, TotalDisk, TotalAllocations, TotalDB, TotalServerCostMonthly] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-            let UserCountrys = [];
-            let ServerProducts = [];
+                Users.map(function(User) {
+                    if(User.suspended === 0){
+                        TotalCoins += User.credits;
+                        TotalAllowedServers += User.server_limit;
 
-            Users.map(function(User) {
-                if(User.suspended === 0){
-                    TotalCoins += User.credits;
-                    TotalAllowedServers += User.server_limit;
+                        if(User.email_verified_at !== null){
+                            TotalVerifiedUsers += 1;
+                        }
 
-                    if(User.email_verified_at !== null){
-                        TotalVerifiedUsers += 1;
+                        if(User.discord_verified_at !== null){
+                            TotalVerifiedDiscordUsers += 1;
+                        }
+
+                        if(User.ip !== null){
+                            UserCountrys.push(geoip.lookup(User.ip).country);
+                        }
+                    }else{
+                        TotalSuspendedUsers += 1;
                     }
+                });
 
-                    if(User.discord_verified_at !== null){
-                        TotalVerifiedDiscordUsers += 1;
+                Servers.map(function(Server) {
+                    if(Server.suspended === null){
+                        Products.map(function(Product) {
+                            if(Server.product_id === Product.id){
+                                TotalCPU += Product.cpu;
+                                TotalRAM += Product.memory;
+                                TotalSWAP += Product.swap;
+                                TotalDisk += Product.disk;
+                                TotalDB += Product.databases;
+                                TotalAllocations += Product.allocations;
+                                TotalServerCostMonthly += Product.price;
+                                ServerProducts.push(Product.name);
+                            } 
+                        })
+                    }else{
+                        ServerSuspended += 1;
                     }
+                });
+                
+                const UserCountrysObject = UserCountrys.reduce(function (acc, curr) {
+                    return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+                }, {})
 
-                    if(User.ip !== null){
-                        UserCountrys.push(geoip.lookup(User.ip).country);
+                const ServerProductsObject = ServerProducts.reduce(function (acc, curr) {
+                    return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+                }, {})
+
+                resolve({
+                    Users: {
+                        TotalUsers: Users.length,
+                        TotalCoins: TotalCoins,
+                        TotalSuspendedUsers: TotalSuspendedUsers,
+                        TotalAllowedServers: TotalAllowedServers,
+                        TotalVerifiedUsers: TotalVerifiedUsers,
+                        TotalVerifiedDiscordUsers: TotalVerifiedDiscordUsers,
+                        CountrysCount: Object.keys(UserCountrysObject).length
+
+                    },
+                    Servers: {
+                        TotalServers: Servers.length,
+                        TotalServerSuspended: ServerSuspended,
+                        TotalCPU: TotalCPU,
+                        TotalRAM: TotalRAM,
+                        TotalSWAP: TotalSWAP,
+                        TotalDisk: TotalDisk,
+                        TotalDB: TotalDB,
+                        TotalAllocations: TotalAllocations,
+                        TotalServerCostMonthly: TotalServerCostMonthly,
+                        ProductsCountInUse: Object.keys(ServerProductsObject).length,
+                        ProductsCount: Products.length
+                    },
+                    Countrys: UserCountrysObject,
+                    Products: ServerProductsObject,
+                    Jobs: {
+                        Failed: FailedJobs[0]['COUNT(*)'],
                     }
-                }else{
-                    TotalSuspendedUsers += 1;
-                }
+                });
+            }).catch(function(err) {
+                console.log(err);
             });
-
-            Servers.map(function(Server) {
-                Products.map(function(Product) {
-                    if(Server.product_id === Product.id){
-                        TotalCPU += Product.cpu;
-                        TotalRAM += Product.memory;
-                        TotalSWAP += Product.swap;
-                        TotalDisk += Product.disk;
-                        TotalDB += Product.databases;
-                        TotalAllocations += Product.allocations;
-                        TotalServerCostMonthly += Product.price;
-                        ServerProducts.push(Product.name);
-                    }
-                })
-            });
-
-            resolve({
-                Users: {
-                    Total: Users.length,
-                    TotalCoins: TotalCoins,
-                    TotalSuspendedUsers: TotalSuspendedUsers,
-                    TotalAllowedServers: TotalAllowedServers,
-                    TotalVerifiedUsers: TotalVerifiedUsers,
-                    TotalVerifiedDiscordUsers: TotalVerifiedDiscordUsers,
-                    Countrys: UserCountrys.reduce(function (acc, curr) {
-                        return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
-                    }, {}),
-                    CountrysCount: UserCountrys.length
-                },
-                Servers: {
-                    Total: Servers.length,
-                    TotalCPU: TotalCPU,
-                    TotalRAM: TotalRAM,
-                    TotalSWAP: TotalSWAP,
-                    TotalDisk: TotalDisk,
-                    TotalDB: TotalDB,
-                    TotalAllocations: TotalAllocations,
-                    TotalServerCostMonthly: TotalServerCostMonthly,
-                    Products: ServerProducts.reduce(function (acc, curr) {
-                        return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
-                    }, {}),
-                    ProductsCount: ServerProducts.length
-                },
-                Jobs: {
-                    Failed: FailedJobs[0]['COUNT(*)'],
-                }
-            });
-        }).catch(function(err) {
-            console.log(err);
-        });
+        } catch (error) {
+            reject(error);
+        }
     });
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-const StatsCollector = require('./collectStats');
-/*
+const { StatsCollector } = require('./collectStats');
+
 const Influxdb = require('influxdb-v2');
-*/
-/*
+
 const db = new Influxdb({
 	host: process.env.Influx_Host,
 	protocol: process.env.Influx_Protocol,
 	port: process.env.Influx_Port,
 	token: process.env.Influx_Token
 });
-*/
 
-StatsCollector.StatsCollector().then(function(result) {
-    console.log(result);
-});
+async function writeDatapoint(key, value) {
+    await db.write(
+        {
+            precision: 's',
+            bucket: process.env.bucket,
+            org: process.env.orga
+        }, [{
+            measurement: key,
+            tags: {host: process.env.host},
+            fields: value
+        }]
+    )
+}
+
+function gather_and_save_stats() {
+    StatsCollector().then(function(result) {
+        for (const [key, value] of Object.entries(result)) {
+            writeDatapoint(key, value);
+        }
+        console.log('Stats saved');
+    });
+}
+gather_and_save_stats();
+setInterval(gather_and_save_stats, process.env.CheckDelayInMS);
